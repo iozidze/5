c9b2e6bb8d7463d1e1abb6d871ce073ec1bf368f
iozidze <akadamenko@mail.ru>
2025-11-11 16:26:30 +0300

    dbingnore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
